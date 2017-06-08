2f60d7fb075251f590f2e243acb2c985add9a5a9
Ryan Sandridge <ryan@sandridgelabs.com>
2017-06-08 13:06:49 -0400

    Game incomplete. Initial structure and prompting started.

---
+++ main.py
@@ -0,0 +1,42 @@
+from collections import namedtuple
+from itertools import cycle
+
+Player = namedtuple('Player', 'name, mark')
+PROMT_BOARD = ((0, 1, 2), (3, 4, 5), (6, 7, 8))
+
+
+def display(board):
+    col_str = ' {} | {} | {} '
+    row_str = '\n-----------\n'
+    print(row_str.join([col_str.format(*row) for row in board]))
+
+
+def print_header():
+    print('------------------------------------------------------------------')
+    print('--------------------- WELCOME TO TIC-TAC-TOE ---------------------')
+    print('------------------------------------------------------------------')
+
+
+def prompt_player(player):
+    print('\n{}, where do you wish to put your {}?'.format(*player))
+    display(PROMT_BOARD)
+    return input('Enter 0-8 or [Q]uit: ')
+
+
+def get_players():
+    x = Player(input("Who will play X's? "), 'X')
+    o = Player(input("Who will play O's? "), 'O')
+    return (x, o)
+
+
+def play_tic_tac_toe():
+    players = get_players()
+    for player in cycle(players):
+        choice = prompt_player(player)
+        if choice == 'Q':
+            break
+
+
+if __name__ == '__main__':
+    print_header()
+    play_tic_tac_toe()
