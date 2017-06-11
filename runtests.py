bcdcfc7e965da2754c08f87c8b0924a875e2fa0e
Ryan Sandridge <ryan@sandridgelabs.com>
2017-06-11 08:59:06 -0400

    Adds a couple automated tests.

---
+++ runtests.py
@@ -0,0 +1,13 @@
+from board import Board
+import player as plyr
+
+def test_board_displays_new_game():
+    b = Board()
+    display = b.display()
+    expected = ' 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n'
+    assert display == expected
+
+
+def test_computer_makes_selection():
+    choice = plyr.Computer('X').choose_play([1, 5, 9])
+    assert choice in ['1', '5', '9']
