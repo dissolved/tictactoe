a80e728272c6a3629fd7366ef1dbdd485ceeca10
Ryan Sandridge <ryan@sandridgelabs.com>
2017-06-11 12:59:33 -0400

    Conform to PEP8.

--- runtests.py
+++ runtests.py
@@ -1,10 +1,16 @@
+"""Run using: py.test runtests.py"""
 from board import Board
 import player as plyr
+
 
 def test_board_displays_new_game():
     b = Board()
     display = b.display()
-    expected = ' 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n'
+    expected = ' 1 | 2 | 3 \n'\
+               '-----------\n'\
+               ' 4 | 5 | 6 \n'\
+               '-----------\n'\
+               ' 7 | 8 | 9 \n'
     assert display == expected
 
 
